68f36b781f687b258a00c5f5d787050516e3a672
beerking36230 <beerking36230@gmail.com>
2023-03-13 14:07:32 +0900

    campingmap third commit2

--- campingmap.js
+++ campingmap.js
@@ -1,7 +1,7 @@
 const API_KEY = 'bsau4CZ2%2FCrD0c%2F%2BogGB12VyPtsUeBBOFUteULnfYcBpmDfH1O576AbDQDlmuutpWnVjQvGR%2BBU%2BEd5pbrERoA%3D%3D';
 
 async function getData() {
-    const url = `https://apis.data.go.kr/B551011/GoCamping/searchList?numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=TEST&ServiceKey=${API_KEY}&keyword=%EC%95%BC%EC%98%81%EC%9E%A5`;
+    const url = `http://apis.data.go.kr/B551011/GoCamping/searchList?numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=TEST&ServiceKey=${API_KEY}&keyword=%EC%95%BC%EC%98%81%EC%9E%A5`;
 
     const response = await fetch(url);
     const text = await response.text();
