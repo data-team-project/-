fbb2de81051985696147386e317cc68bceb4f9d2
beerking36230 <beerking36230@gmail.com>
2023-03-13 15:58:07 +0900

    campingmap 11th commit

--- campingmap.js
+++ campingmap.js
@@ -10,12 +10,13 @@ function initMap() {
 }
 
 async function getData(map) {
-  const url = `http://apis.data.go.kr/B551011/GoCamping/locationBasedList?MobileOS=ETC&MobileApp=TEST&serviceKey=${API_KEY}&_type=json&mapX=128.6142847&mapY=36.0345423&radius=2000`;
+  const url = `https://apis.data.go.kr/B551011/GoCamping/locationBasedList?MobileOS=etc&MobileApp=test&serviceKey=bsau4CZ2%252FCrD0c%252F%252BogGB12VyPtsUeBBOFUteULnfYcBpmDfH1O576AbDQDlmuutpWnVjQvGR%252BBU%252BEd5pbrERoA%253D%253D&mapX=128.6142847&mapY=36.0345423&radius=2000`;
 
   const response = await fetch(url);
   const data = await response.json();
 
   console.log(data); // 데이터 출력
+  console.log(data.response.body.items.item)
 
   // 마커 생성 및 지도에 추가
   data.response.body.items.item.forEach(item => {
