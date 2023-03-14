4664e2606bbee25c5e44b4adad009ffba64f2d01
beerking36230 <beerking36230@gmail.com>
2023-03-14 15:52:11 +0900

    campingmap final commit

--- campingmap.js
+++ campingmap.js
@@ -1,8 +1,8 @@
 async function getData() {
-  const url = `https://apis.data.go.kr/B551011/GoCamping/locationBasedList?&MobileOS=etc&MobileApp=test&serviceKey=bsau4CZ2%2FCrD0c%2F%2BogGB12VyPtsUeBBOFUteULnfYcBpmDfH1O576AbDQDlmuutpWnVjQvGR%2BBU%2BEd5pbrERoA%3D%3D&numOfRows=20&mapX=128.6142847&mapY=36.0345423&radius=20000&_type=json`;
+  const url = `https://apis.data.go.kr/B551011/GoCamping/locationBasedList?&MobileOS=etc&MobileApp=test&serviceKey=bsau4CZ2%2FCrD0c%2F%2BogGB12VyPtsUeBBOFUteULnfYcBpmDfH1O576AbDQDlmuutpWnVjQvGR%2BBU%2BEd5pbrERoA%3D%3D&numOfRows=10000&mapX=126.977012&mapY=37.5347189&radius=2000000&_type=json`;
   const response = await fetch(url);
   const data = await response.json();
-  const locations = data.response.body.items.item.map(spot => [spot.facltNm, spot.mapX, spot.mapY, spot.intro]);
+  const locations = data.response.body.items.item.map(spot => [spot.facltNm, spot.mapY, spot.mapX, spot.lineIntro]);
 
   console.log("data", data);
   console.log("locations", locations);
@@ -26,22 +26,19 @@ function drawMap(locations) {
       position: new google.maps.LatLng(locations[i][1], locations[i][2]),
       map: map,
     });
-
-    google.maps.event.addListener(
-      marker,
-      "click",
-      (function (marker, i) {
-        return function () {
-          infowindow.setContent(locations[i][0]);
-          infowindow.setContent(locations[i][3]);
-          infowindow.open(map, marker);
-        };
-      })(marker, i)
-    );
+  
+    google.maps.event.addListener(marker, "click", (function(marker, i) {
+      const infoContent = `<div><strong>${locations[i][0]}</strong><br>
+                            ${locations[i][3]}<br>
+                            <a href="https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(locations[i][0])}&destination_place_id=${encodeURIComponent(locations[i][4])}&travelmode=transit" target="_blank">가는 방법 구글 지도로 확인하기</a></div>`;
+      return function() {
+        infowindow.setContent(infoContent);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
   }
 }
 
 function initMap() {
-  // 지도 초기화 및 표시하는 로직 작성
   getData();
 }
