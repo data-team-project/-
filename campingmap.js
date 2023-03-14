79ecc35a02187c866df317d59db2962b8d1abe75
beerking36230 <beerking36230@gmail.com>
2023-03-14 09:50:31 +0900

    campingmap 12th commit

--- campingmap.js
+++ campingmap.js
@@ -1,50 +1,20 @@
-const API_KEY = 'bsau4CZ2%2FCrD0c%2F%2BogGB12VyPtsUeBBOFUteULnfYcBpmDfH1O576AbDQDlmuutpWnVjQvGR%2BBU%2BEd5pbrERoA%3D%3D';
-
-function initMap() {
-  const map = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
+function addMarkerToMap(lat, lng) {
+  var marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(lat, lng),
+    map: map
   });
-
-  getData(map);
 }
 
-async function getData(map) {
-  const url = `https://apis.data.go.kr/B551011/GoCamping/locationBasedList?MobileOS=etc&MobileApp=test&serviceKey=bsau4CZ2%252FCrD0c%252F%252BogGB12VyPtsUeBBOFUteULnfYcBpmDfH1O576AbDQDlmuutpWnVjQvGR%252BBU%252BEd5pbrERoA%253D%253D&mapX=128.6142847&mapY=36.0345423&radius=2000`;
-
+async function getData() {
+  const url = `https://apis.data.go.kr/B551011/GoCamping/locationBasedList?MobileOS=etc&MobileApp=test&serviceKey=bsau4CZ2%2FCrD0c%2F%2BogGB12VyPtsUeBBOFUteULnfYcBpmDfH1O576AbDQDlmuutpWnVjQvGR%2BBU%2BEd5pbrERoA%3D%3D&mapX=128.6142847&mapY=36.0345423&radius=2000&_type=json`;
   const response = await fetch(url);
   const data = await response.json();
+  console.log("data", data);
 
-  console.log(data); // 데이터 출력
-  console.log(data.response.body.items.item)
-
-  // 마커 생성 및 지도에 추가
-  data.response.body.items.item.forEach(item => {
-    const lat = item.mapY;
-    const lng = item.mapX;
-
-    const marker = new naver.maps.Marker({
-      position: new naver.maps.LatLng(lat, lng),
-      map: map,
-      icon: {
-        content: [
-          '<div class="marker">',
-          '<div class="marker-icon"></div>',
-          '</div>'
-        ].join(''),
-        anchor: new naver.maps.Point(11, 35)
-      }
-    });
-
-    // 마커 클릭 시 정보 창 열기
-    const content = `<div><h4>${item.facltNm}</h4><p>${item.addr1}</p></div>`;
-    const infoWindow = new naver.maps.InfoWindow({
-      content: content
-    });
-    naver.maps.Event.addListener(marker, 'click', () => {
-      infoWindow.open(map, marker);
-    });
+  // 받아온 데이터에서 좌표값 추출하여 지도에 마커 추가
+  data.locationBasedList.forEach(function(item) {
+    let lat = parseFloat(item.mapY);
+    let lng = parseFloat(item.mapX);
+    addMarkerToMap(lat, lng);
   });
-
-  console.log(data.response.body.items.item);
 }
